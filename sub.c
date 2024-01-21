4f70a24f922c412815c70db5a8e750154a172157
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-01-21 11:09:24 +0300

    task

---
+++ sub.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * sub - subtracts top two elements
+ * @top: address of the first node
+ * @line: line number
+ *
+ */
+void sub(stack_t **top, unsigned int line_number)
+{
+	int value;
+	stack_t *newNode;
+
+	if (*top == NULL || (*top)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stackt too short\n", line_number);
+		fclose(container.file);
+		free(container.line);
+		exit(EXIT_FAILURE);
+	}
+	newNode = *top;
+	value = (*top)->n - (*top)->next->n;
+	newNode = (*top)->next;
+	free(*top);
+	*top = newNode;
+	(*top)->n = value;
+}
