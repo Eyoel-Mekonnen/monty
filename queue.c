b702999e05ce519b8abde944b65dbe170500b59f
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2024-04-07 14:32:56 +0300

    tasks

---
+++ queue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+/**
+ * queue - operations perform the opposite of stack
+ * @top: top of the address
+ * @line_number: returns number of line
+ *
+ */
+void queue(stack_t **top, unsigned int line_number)
+{
+	stack_t *tracker;
+
+	tracker = *top;
+	
+}
